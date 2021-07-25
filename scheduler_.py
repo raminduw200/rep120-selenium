cac44ab31d767e4c300b1b824ef8cb8aa651b7b1
Ramindu <raminduw200@gmail.com>
2021-07-25 21:54:45 +0530

    Schedule timetable at once

--- scheduler_.py
+++ scheduler_.py
@@ -1,44 +1,46 @@
 import pandas as pd
-import time
 import datetime
 import schedule
-
+from tabulate import tabulate
 from rep120 import rep120_selenium as rep120
 
 df = pd.read_csv('time-table.csv')
-week_no = datetime.datetime.now().weekday()
-
-day_courses = df[[df.columns[0], df.columns[week_no]]].copy()
-today = day_courses.columns[1]
-day_courses = day_courses.dropna(subset=['-', today])
+# week_no = datetime.datetime.now().weekday()
+output = []
 
 
 def schedule_timetable():
-    if week_no == 0:
-        for time_, subj_ in zip(day_courses['-'], day_courses[today]):
-            schedule.every().monday.at(time_).do(rep120, str(int(subj_)))
-            print(f"{time_} : {subj_} : {type(subj_)}")
-    elif week_no == 1:
-        for time_, subj_ in zip(day_courses['-'], day_courses[today]):
-            schedule.every().tuesday.at(time_).do(rep120, str(int(subj_)))
-            print(f"{time_} : {subj_} : {type(subj_)}")
-    elif week_no == 2:
-        for time_, subj_ in zip(day_courses['-'], day_courses[today]):
-            schedule.every().wednesday.at(time_).do(rep120, str(int(subj_)))
-            print(f"{time_} : {subj_} : {type(subj_)}")
-    elif week_no == 3:
-        for time_, subj_ in zip(day_courses['-'], day_courses[today]):
-            schedule.every().thursday.at(time_).do(rep120, str(int(subj_)))
-            print(f"{time_} : {subj_} : {type(subj_)}")
-    elif week_no == 4:
-        for time_, subj_ in zip(day_courses['-'], day_courses[today]):
-            schedule.every().friday.at(time_).do(rep120, str(int(subj_)))
-            print(f"{time_} : {subj_} : {type(subj_)}")
-    # elif week_no == 5:
-    #     for time_, subj_ in zip(day_courses['-'], day_courses[today]):
-    #         schedule.every().saturday.at(time_).do(rep120, str(int(subj_)))
-    #         print(f"{time_} : {subj_} : {type(subj_)}")
-    # elif week_no == 6:
-    #     for time_, subj_ in zip(day_courses['-'], day_courses[today]):
-    #         schedule.every().sunday.at(time_).do(rep120, str(int(subj_)))
-    #         print(f"{time_} : {subj_} : {type(subj_)}")
+    for week_day in range(5):
+        day_courses = df[[df.columns[0], df.columns[week_day + 1]]].copy()
+        day = day_courses.columns[1]
+        day_courses = day_courses.dropna(subset=['-', day])
+
+        if week_day == 0:
+            for time_, subj_ in zip(day_courses['-'], day_courses[day]):
+                schedule.every().monday.at(time_).do(rep120, str(int(subj_)))
+                output.append(["Monday", time_, subj_, "Scheduled"])
+        elif week_day == 1:
+            for time_, subj_ in zip(day_courses['-'], day_courses[day]):
+                schedule.every().tuesday.at(time_).do(rep120, str(int(subj_)))
+                output.append(["Tuesday", time_, subj_, "Scheduled"])
+        elif week_day == 2:
+            for time_, subj_ in zip(day_courses['-'], day_courses[day]):
+                schedule.every().wednesday.at(time_).do(rep120, str(int(subj_)))
+                output.append(["Wednesday", time_, subj_, "Scheduled"])
+        elif week_day == 3:
+            for time_, subj_ in zip(day_courses['-'], day_courses[day]):
+                schedule.every().thursday.at(time_).do(rep120, str(int(subj_)))
+                output.append(["Thursday", time_, subj_, "Scheduled"])
+        elif week_day == 4:
+            for time_, subj_ in zip(day_courses['-'], day_courses[day]):
+                schedule.every().friday.at(time_).do(rep120, str(int(subj_)))
+                output.append(["Friday", time_, subj_, "Scheduled"])
+        # elif week_day == 5:
+        #     for time_, subj_ in zip(day_courses['-'], day_courses[day]):
+        #         schedule.every().saturday.at(time_).do(rep120, str(int(subj_)))
+        #         table.append(["Saturday", time_, subj_, "Scheduled"])
+        # elif week_day == 6:
+        #     for time_, subj_ in zip(day_courses['-'], day_courses[day]):
+        #         schedule.every().sunday.at(time_).do(rep120, str(int(subj_)))
+        #         table.append(["Sunday", time_, subj_, "Scheduled"])
+    print(tabulate(output, headers=["Day", "Time", "Subject", "Schedule"]))
